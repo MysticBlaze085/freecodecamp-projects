dd91d53ef2d106163610859028a326b27a4d2fe2
Melissa Langhausen <melissaslanghausen@gmail.com>
2020-08-08 10:45:57 -0500

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './modules/shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigationComponent } from './modules/shared';
+import { FlexLayoutModule } from '@angular/flex-layout/typings/module';
 
 @NgModule({
     declarations: [AppComponent, NavigationComponent],
-    imports: [BrowserModule, AppRoutingModule, SharedModule, BrowserAnimationsModule],
+    imports: [BrowserModule, AppRoutingModule, SharedModule, BrowserAnimationsModule, FlexLayoutModule],
     providers: [],
     bootstrap: [AppComponent],
 })
